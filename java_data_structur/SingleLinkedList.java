7a867a45edd1fa14ebac0ae2328aa6e7ece59b5a
KimTaeyoun <kimty1121@gmail.com>
2021-09-09 23:06:29 +0900

    addNodeInside

--- java_data_structur/SingleLinkedList.java
+++ java_data_structur/SingleLinkedList.java
@@ -33,11 +33,40 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public Node<T> search(T data) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while(node != null) {
+                if (node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.search(isData);
+
+        if (searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+
     public static void main(String[] args) {
         SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<Integer>();
         MyLinkedList.addNode(1);
         MyLinkedList.addNode(2);
         MyLinkedList.addNode(3);
+        MyLinkedList.addNodeInside(5, 1);
 
         MyLinkedList.printAll();
     }

63d36d75a92d78465588e0aa3677447921d4b583
KimTaeyoun <kimty1121@gmail.com>
2021-09-14 22:25:24 +0900

    add insertToFront

--- java_data_structur/DoubleLinkedList.java
+++ java_data_structur/DoubleLinkedList.java
@@ -7,12 +7,20 @@ public class DoubleLinkedList<T> {
         MyLinkedList.addNode(3);
         MyLinkedList.addNode(4);
         MyLinkedList.addNode(5);
-
         MyLinkedList.printAll();
+        System.out.println("----------------");
 
-        System.out.println(MyLinkedList.searchFromHead(3));
-        System.out.println(MyLinkedList.searchFromTail(1));
-        System.out.println(MyLinkedList.searchFromTail(6));
+        MyLinkedList.insertToFront(3, 2);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.insertToFront(6, 2);
+        MyLinkedList.insertToFront(1, 0);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.addNode(6);
+        MyLinkedList.printAll();
     }
 
     public Node<T> head = null;
@@ -83,6 +91,36 @@ public class DoubleLinkedList<T> {
                 }
             }
             return null;
+        }
+    }
+    
+    public boolean insertToFront(T existedData, T addData) {
+        if (this.head == null) {
+            this.head = new Node<T>(addData);
+            this.tail = this.head;
+            return true;
+        } else if (this.head.data == existedData) {
+            Node<T> newHead = new Node<T>(addData);
+            newHead.next = this.head;
+            this.head = newHead;
+            return true;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == existedData) {
+                    Node<T> nodePrev = node.prev;
+                    
+                    nodePrev.next = new Node<T>(addData);
+                    nodePrev.next.next = node;
+                    
+                    nodePrev.next.prev = nodePrev;
+                    node.prev = nodePrev.next;
+                    return true;
+                } else {
+                    node = node.next;
+                }
+            }
+            return false;
         }
     }
 }

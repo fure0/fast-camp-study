7dabebdb80cee7f20fd593932ab9f304f97b11f4
KimTaeyoun <kimty1121@gmail.com>
2021-09-19 02:55:22 +0900

    train more

--- java_data_structur/training/Hash_Chaining.java
+++ java_data_structur/training/Hash_Chaining.java
@@ -1,5 +1,5 @@
 public class Hash_Chaining {
-    
+
     public class Slot {
         String key;
         String value;
@@ -21,24 +21,22 @@ public class Hash_Chaining {
         return (int)(key.charAt(0)) % this.hashTable.length;
     }
 
-    public boolean saveData(String key, String value) {
+    public boolean SaveData(String key, String value) {
         Integer address = this.hashFunc(key);
-        System.out.println(address);
         if (this.hashTable[address] != null) {
-            Slot findSlot = this.hashTable[address]; //DaveLee
-            Slot prevSlot = this.hashTable[address]; //DaveLee
-            while (findSlot != null ) { //DaveLee
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+            while (findSlot != null) {
                 if (findSlot.key == key) {
-                    findSlot.value = value; //update data
-                    return true;
+                    findSlot.value = value;
                 } else {
-                    prevSlot = findSlot; //DaveLee
-                    findSlot = findSlot.next; //null David
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
                 }
+                prevSlot.next = new Slot(key, value);
             }
-            prevSlot.next = new Slot(key, value); //David
         } else {
-            this.hashTable[address] = new Slot(key, value); //DaveLee
+            this.hashTable[address] = new Slot(key, value);
         }
         return true;
     }
@@ -48,7 +46,7 @@ public class Hash_Chaining {
         if (this.hashTable[address] != null) {
             Slot findSlot = this.hashTable[address];
             while (findSlot != null) {
-                if(findSlot.key == key) {
+                if (findSlot.key == key) {
                     return findSlot.value;
                 } else {
                     findSlot = findSlot.next;
@@ -62,13 +60,11 @@ public class Hash_Chaining {
 
     public static void main(String[] args) {
         Hash_Chaining mainObject = new Hash_Chaining(20);
-        mainObject.saveData("DaveLee", "01022223333");
-        mainObject.saveData("David", "01044445555");
-        mainObject.saveData("Dave", "01055556666");
-        // mainObject.saveData("fun-coding", "01033334444");
-        
-        System.out.println(mainObject.getData("DaveLee"));
-        System.out.println(mainObject.getData("David"));
-        System.out.println(mainObject.getData("Dave"));
+        mainObject.SaveData("kimTony", "01011112222");
+        mainObject.SaveData("kimSoo", "01022223333");
+
+        System.out.println(mainObject.getData("kimTony"));
+        System.out.println(mainObject.getData("kimSoo"));
     }
+    
 }

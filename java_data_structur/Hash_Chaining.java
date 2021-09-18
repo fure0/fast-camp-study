ce8f92fc3ed5bc7a502554d7ba2b6dd4c1987f18
KimTaeyoun <kimty1121@gmail.com>
2021-09-18 22:33:54 +0900

    Chaining Hash

---
+++ java_data_structur/Hash_Chaining.java
@@ -0,0 +1,70 @@
+public class Hash_Chaining {
+    public Slot[] hashTable;
+
+    public Hash_Chaining(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String key;
+        String value;
+        Slot next;
+        Slot(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+            while (findSlot != null ) {
+                if (findSlot.key == key) {
+                    findSlot.value = value;
+                    return true;
+                } else {
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
+                }
+            }
+            prevSlot.next = new Slot(key, value);
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if(findSlot.key == key) {
+                    return findSlot.value;
+                } else {
+                    findSlot = findSlot.next;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Hash_Chaining mainObject = new Hash_Chaining(20);
+        mainObject.saveData("DaveLee", "01022223333");
+        mainObject.saveData("fun-coding", "01033334444");
+        mainObject.saveData("David", "01044445555");
+        mainObject.saveData("Dave", "01055556666");
+        mainObject.getData("Dave");
+        System.out.println(mainObject.getData("fun-coding"));
+    }
+}

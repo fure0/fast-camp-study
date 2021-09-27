0ffa69da918d94a53c8cc2ab9a0292f931dce744
KimTaeyoun <kimty1121@gmail.com>
2021-09-27 22:19:44 +0900

    study heap delete

--- java_data_structur/Heap.java
+++ java_data_structur/Heap.java
@@ -45,6 +45,78 @@ public class Heap {
         return true;
     }
 
+    public boolean move_down(Integer popped_idx) {
+        Integer left_child_popped_idx, right_child_popped_idx;
+
+        left_child_popped_idx = popped_idx * 2;
+        right_child_popped_idx = popped_idx * 2+1;
+
+        // CASE1: 왼쪽 자식 노드도 없을 때
+        if (left_child_popped_idx >= this.heapArray.size()) {
+            return false;
+            // CASE2: 오른쪽 자식 노드만 없을 때
+        } else if (right_child_popped_idx >= this.heapArray.size()) {
+            if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                return true;
+            } else {
+                return false;
+            }
+            // CASE3: 왼쪽, 오른쪽 자식 노드 모두 있을 때
+        } else {
+            if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    public Integer pop() {
+        Integer returned_data, popped_idx, left_child_popped_idx, right_child_popped_idx;
+
+        if (this.heapArray.size() <= 1) {
+            return null;
+        }
+
+        returned_data = this.heapArray.get(1);
+        this.heapArray.set(1, this.heapArray.get(this.heapArray.size() - 1));
+        this.heapArray.remove(this.heapArray.size() - 1);
+        popped_idx = 1;
+
+        while (this.move_down(popped_idx)) {
+            left_child_popped_idx = popped_idx * 2;
+            right_child_popped_idx = popped_idx * 2 + 1;
+
+            if (right_child_popped_idx >= this.heapArray.size()) {
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                    Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                    popped_idx = left_child_popped_idx;
+                }
+            } else {
+                if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
+                    if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+                        Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                        popped_idx = left_child_popped_idx;
+                    }
+                } else {
+                    if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
+                        Collections.swap(this.heapArray, popped_idx, right_child_popped_idx);
+                        popped_idx = right_child_popped_idx;
+                    }
+                }
+            }
+        }
+        return returned_data;
+    }
+
     public static void main(String[] args) {
         Heap heapTest = new Heap(15);
         heapTest.insert(10);
@@ -53,6 +125,9 @@ public class Heap {
         heapTest.insert(4);
         heapTest.insert(20);
 
+        System.out.println(heapTest.heapArray);
+
+        heapTest.pop();
         System.out.println(heapTest.heapArray);
     }
 

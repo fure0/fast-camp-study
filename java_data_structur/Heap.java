3f96ad0bf371ad16c39bea3a3a9efc65fb2d7f2d
KimTaeyoun <kimty1121@gmail.com>
2021-09-28 23:55:32 +0900

    add comment to heap

--- java_data_structur/Heap.java
+++ java_data_structur/Heap.java
@@ -7,16 +7,20 @@ public class Heap {
     public Heap (Integer data) {
         heapArray = new ArrayList<Integer>();
 
-        heapArray.add(null);
+        heapArray.add(null); //힙은 1부터 새는게 편하기 때문에 0번째를 Null로
         heapArray.add(data);
     }
 
-    public boolean move_up(Integer inserted_idx) {
-        if (inserted_idx <=1) {
+    // idx tower example
+    //    1
+    //   2 3
+    // 4 5 6 7
+    public boolean move_up(Integer inserted_idx) { //inserted_idx 는 맨 마지막 삽입된 인덱스 this.heapArray.size()-1
+        if (inserted_idx <=1) { // 더이상 올라갈 곳이 없는 경우
             return false;
         }
-        Integer parent_idx = inserted_idx / 2;
-        if (this.heapArray.get(inserted_idx) > this.heapArray.get(parent_idx)) {
+        Integer parent_idx = inserted_idx / 2; // JAVA 에서는 / 연산자로 몫 을 구할 수 있음
+        if (this.heapArray.get(inserted_idx) > this.heapArray.get(parent_idx)) { //삽입된 값이 부모값 보다 크면 스왑해야한다
             return true;
         } else {
             return false;
@@ -37,7 +41,7 @@ public class Heap {
         this.heapArray.add(data);
         inserted_idx = this.heapArray.size()-1;
 
-        while (this.move_up(inserted_idx)) {
+        while (this.move_up(inserted_idx)) { //최후의 삽입된 값이 부모노드 값 보다 크면 스왑
             parent_idx = inserted_idx/2;
             Collections.swap(this.heapArray, inserted_idx, parent_idx);
             inserted_idx = parent_idx;
@@ -82,11 +86,11 @@ public class Heap {
     public Integer pop() {
         Integer returned_data, popped_idx, left_child_popped_idx, right_child_popped_idx;
 
-        if (this.heapArray.size() <= 1) {
+        if (this.heapArray.size() <= 1) { // null을 넣어놨기 떄문에 size가 1보다 적거나 같다면 null 밖에 없거나 데이터가 없는경우
             return null;
         }
 
-        returned_data = this.heapArray.get(1);
+        returned_data = this.heapArray.get(1); // 맨 위에 값 = 최대값
         this.heapArray.set(1, this.heapArray.get(this.heapArray.size() - 1));
         this.heapArray.remove(this.heapArray.size() - 1);
         popped_idx = 1;
@@ -95,20 +99,22 @@ public class Heap {
             left_child_popped_idx = popped_idx * 2;
             right_child_popped_idx = popped_idx * 2 + 1;
 
+            // CASE2: 오른쪽 자식 노드만 없을 때
             if (right_child_popped_idx >= this.heapArray.size()) {
-                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
-                    Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) { // 왼쪽 자식 노드가 부모 노드보다 클 때
+                    Collections.swap(this.heapArray, popped_idx, left_child_popped_idx); //왼쪽 자식 노드를 위로 (부모가 자식 보다 커야되서)
                     popped_idx = left_child_popped_idx;
                 }
+            // CASE3: 왼쪽/오른쪽 자식 노드가 모두 있을 때 
             } else {
-                if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
-                    if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
-                        Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) { //왼쪽 자식노드가 오른쪽 자식노드 보다 클 때
+                    if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) { //왼쪽 자식 노드가 부모 노드보다 클 때
+                        Collections.swap(this.heapArray, popped_idx, left_child_popped_idx); //왼쪽 자식 노드를 위로 (부모가 자식 보다 커야되서)
                         popped_idx = left_child_popped_idx;
                     }
-                } else {
-                    if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
-                        Collections.swap(this.heapArray, popped_idx, right_child_popped_idx);
+                } else { //오른쪽 자식노드가 왼쪽 자식노드 보다 클 때
+                    if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {  //오른쪽 자식 노드가 부모 노드보다 클 때
+                        Collections.swap(this.heapArray, popped_idx, right_child_popped_idx); //오른쪽 자식 노드를 위로 (부모가 자식 보다 커야되서)
                         popped_idx = right_child_popped_idx;
                     }
                 }
